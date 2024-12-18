db6a4dd233230a2c06cfa33caa76310975422db5
GersonEC <gersonenriquez94@gmail.com>
2024-12-18 15:43:21 +0100

    get tickets from db

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,14 +1,15 @@
 import { Heading } from '@/components/Heading';
 import { TicketItem } from '@/features/ticket/components/TicketItem';
-import { initialTickets } from '@/lib/data';
+import { getTickets } from '@/features/ticket/queries/getTickets';
 
-const TicketsPage = () => {
+const TicketsPage = async () => {
+  const tickets = await getTickets();
   return (
     <div className='flex-1 flex flex-col gap-y-8'>
       <Heading title='Tickets' description='All your tickets at one place' />
 
       <div className='flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top'>
-        {initialTickets.map((ticket) => (
+        {tickets.map((ticket) => (
           <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
---
+++ src/features/ticket/queries/getTickets.ts
@@ -0,0 +1,6 @@
+import { Ticket } from '@prisma/client';
+import { prisma } from '@/lib/prisma';
+
+export const getTickets = async (): Promise<Ticket[]> => {
+  return await prisma.ticket.findMany();
+};
