6b59e22b18f2547c17e436437b8e4bafecb292ee
GersonEC <gersonenriquez94@gmail.com>
2024-12-18 16:11:30 +0100

    refactor get tickets apis

--- src/features/ticket/queries/getTicket.ts
+++ src/features/ticket/queries/getTicket.ts
@@ -1,11 +1,10 @@
-import { initialTickets } from '@/lib/data';
-import { Ticket } from '../types';
+import { Ticket } from '@prisma/client';
+import { prisma } from '@/lib/prisma';
 
 export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  const maybeTicket = initialTickets.find((ticket) => ticket.id === ticketId);
-  return new Promise((resolve) => {
-    resolve(maybeTicket || null);
+  return await prisma.ticket.findUnique({
+    where: {
+      id: ticketId,
+    },
   });
 };
--- src/features/ticket/queries/getTickets.ts
+++ src/features/ticket/queries/getTickets.ts
@@ -2,5 +2,9 @@ import { Ticket } from '@prisma/client';
 import { prisma } from '@/lib/prisma';
 
 export const getTickets = async (): Promise<Ticket[]> => {
-  return await prisma.ticket.findMany();
+  return await prisma.ticket.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
 };
